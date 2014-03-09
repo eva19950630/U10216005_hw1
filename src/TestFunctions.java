6d16a5188b1ba5ab6c3909c2153548542003606d
eva19950630 <fun910347@gmail.com>
2014-03-09 11:49:09 +0800

    我先把parent class寫好
    用了abstract class

--- src/TestFunctions.java
+++ src/TestFunctions.java
@@ -3,6 +3,7 @@ public class TestFunctions {
 
 	public static void main(String[] args) {
 		
+
 	}
 
 }
@@ -38,44 +39,5 @@ abstract class Functions {
 	public abstract double getFunctionE();
 	public abstract double getFunctionF();
 	public abstract double getFunctionG();
-	
-}
-
-class FunctionExample extends Functions {
-
-	@Override
-	public double getFunctionA() {
-		return Math.pow(x,x);
-	}
-
-	@Override
-	public double getFunctionB() {
-		return Math.sin(x);
-	}
-
-	@Override
-	public double getFunctionC() {
-		return Math.cos(x);
-	}
-
-	@Override
-	public double getFunctionD() {
-		return Math.tan(x);
-	}
-
-	@Override
-	public double getFunctionE() {
-		return Math.cos(x) + 5 * Math.sin(x);
-	}
-
-	@Override
-	public double getFunctionF() {
-		return 5 * Math.cos(x) + Math.sin(x);
-	}
-
-	@Override
-	public double getFunctionG() {
-		return Math.log(x) + Math.pow(x,x);
-	}
 	
 }
