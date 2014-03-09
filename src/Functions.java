dc34b26b91954b294e7929fec16550d6ed7419c8
eva19950630 <fun910347@gmail.com>
2014-03-09 11:26:42 +0800

    我先把parent class寫好
    用了abstract class

---
+++ src/Functions.java
@@ -0,0 +1,34 @@
+
+public abstract class Functions {
+
+	private int x;
+	
+	protected Functions() {
+	}
+	
+	protected Functions(int x) {
+		this.x = x;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	@Override
+	public String toString() {
+		return "The answer is " + x;
+	}
+	
+	public abstract double getFunctionA();
+	public abstract double getFunctionB();
+	public abstract double getFunctionC();
+	public abstract double getFunctionD();
+	public abstract double getFunctionE();
+	public abstract double getFunctionF();
+	public abstract double getFunctionG();
+	
+}
